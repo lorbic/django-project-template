c30b004f93c4449dc71a5e27f5077caaca87b256
Vikash <vikaspatelp83@gmail.com>
2022-12-04 02:21:06 +0530

    Add static root settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -150,6 +150,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "public"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
+
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
