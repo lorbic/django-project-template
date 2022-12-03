6df127b2ffdedecc12889df54ebe6366c2650a05
Vikash <vikaspatelp83@gmail.com>
2022-12-04 02:31:09 +0530

    Fix static root

--- core/settings/base.py
+++ core/settings/base.py
@@ -150,9 +150,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / "public"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles"
